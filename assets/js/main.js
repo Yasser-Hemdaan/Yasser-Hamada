1b95e6fba0ca3159a679123d7de3623dfdf14cc8
Yasser-Hemdaan <yasserhemdaan@gmail.com>
2025-04-16 21:01:22 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,545 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".about .block", 2, {
+  scrollTrigger: {
+    trigger: ".about",
+    start: "top top",
+    end: "bottom center",
+    // markers: true,
+    scrub: true,
+    pin: true,
+  },
+  y: "-82.5vh",
+});
+
+gsap.to(".stack .shapes", 2, {
+  scrollTrigger: {
+    trigger: ".stack",
+    start: "top center",
+    end: "bottom 90%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.5,
+  y: "60vh",
+});
+
+gsap.to(".stack .card1", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card1",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card2", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card2",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card3", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card3",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card4", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card4",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card5", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card5",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card6", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card6",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card7", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card7",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card8", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card8",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card9", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card9",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card10", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card10",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card11", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card11",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card12", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card12",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card13", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card13",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card14", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card14",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card15", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card15",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card16", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card16",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card17", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card17",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card18", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card18",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card19", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card19",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card20", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card20",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card21", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card21",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card22", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card22",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+gsap.to(".stack .card23", 2, {
+  scrollTrigger: {
+    trigger: ".stack .card23",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+
+gsap.to(".services .service1 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service1 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service2 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service2 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service3 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service3 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service4 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service4 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service5 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service5 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service6 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service6 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service7 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service7 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service8 .content", 2, {
+  scrollTrigger: {
+    trigger: ".services .service8 .content",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+gsap.to(".services .service1 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service1 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service2 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service2 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service3 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service3 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service4 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service4 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service5 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service5 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service6 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service6 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service7 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service7 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+gsap.to(".services .service8 .title", 2, {
+  scrollTrigger: {
+    trigger: ".services .service8 .title",
+    start: "top 85%",
+    end: "bottom 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplayHoverPause: true,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
+
+gsap.to(".projects .project1", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project1",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project2", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project2",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project3", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project3",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project4", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project4",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project5", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project5",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project6", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project6",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project7", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project7",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+gsap.to(".projects .project8", 2, {
+  scrollTrigger: {
+    trigger: ".projects .project8",
+    start: "top 85%",
+    end: "65% 60%",
+    // markers: true,
+    scrub: true,
+  },
+  opacity: 1,
+  rotate: 0,
+});
+
+gsap.to(".footer .name", 2, {
+  scrollTrigger: {
+    trigger: ".footer",
+    start: "10% 90%",
+    end: "bottom bottom",
+    // markers: true,
+    scrub: true,
+  },
+  y: "-22vw",
+  opacity: 1,
+});
